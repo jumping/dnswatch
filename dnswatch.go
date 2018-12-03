b994499c58a3a563848756cb8d9aac46faa7a1b5
Jumping Qu <jumping.qu@JQ.local>
2018-12-03 15:46:03 +0800

    added get all devices

--- dnswatch.go
+++ dnswatch.go
@@ -3,17 +3,55 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"time"
 
-	"code.google.com/p/gopacket"
-	"code.google.com/p/gopacket/layers"
-	"code.google.com/p/gopacket/pcap"
+	//"github.com/davecgh/go-spew/spew"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
+
+// yum install libpcap-1.4.0
+
+func device() (ret []string) {
+	fmt.Println("----------Find all devices---------\n ")
+
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print device information
+	for _, device := range devices {
+		if len(device.Addresses) < 1 {
+			continue
+		}
+		fmt.Println(device.Name)
+		for _, address := range device.Addresses {
+			if len(address.IP) != 4 {
+				continue
+			}
+			fmt.Println("- IP address: ", address.IP)
+			fmt.Println("- Subnet mask: ", address.Netmask)
+			if fmt.Sprint(address.IP) != "127.0.0.1" {
+				ret = append(ret, device.Name)
+			}
+		}
+	}
+	return
+}
 
 func main() {
 	var deviceInterface string
 	flag.StringVar(&deviceInterface, "interface", "eth0", "device interface to listen for DNS packets")
 	flag.Parse()
+
+	ret := device()
+	if len(ret) > 0 {
+		fmt.Println(ret)
+		deviceInterface = ret[0]
+	}
 
 	handle, err := pcap.OpenLive(deviceInterface, 65535, true, 1000)
 	if err != nil {
@@ -39,6 +77,7 @@ func main() {
 func handlePacket(packet gopacket.Packet) {
 	metadata := packet.Metadata()
 	dnsLayer := packet.Layer(layers.LayerTypeDNS)
+	//arpLayer := packet.Layer(layers.LayerTypeARP)
 	dns, _ := dnsLayer.(*layers.DNS)
 	if len(dns.Answers) == 0 {
 		return
